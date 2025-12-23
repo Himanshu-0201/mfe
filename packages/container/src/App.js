ac1960bb4bc8857c2ef86dab93260feecb582d48
Himanshu Nagar <nagar.himanshu.1802@gmail.com>
2025-12-23 21:25:19 +0530

    Changed the content of the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
     return (
         <div>
-            <h1> Hi Himanshu</h1>
+            <h1> Hi Himanshu, I am hosted from the Amazon S3</h1>
             <MarketingApp />
         </div>
     )
